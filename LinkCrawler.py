37482b666803e23dfc8a5c606b2f2cd91eade845
Scott Hall <62972261+scotthall6@users.noreply.github.com>
2022-01-13 01:41:25 -0700

    Update LinkCrawler.py

--- LinkCrawler.py
+++ LinkCrawler.py
@@ -2,22 +2,24 @@
 This python script provides a simple way to check a website for broken links and report them to a .csv file.
 Release: v1.0-beta1
 
----Dependencies---
+-------------------------------------Dependencies---------------------------------------
 Scrapy (https://scrapy.org)
 v1.0-beta: Unix environment
 
----Implementation---
+-------------------------------------Implementation-------------------------------------
 ) Create target directory & cd to empty directory. NOTE: Target directory MUST be empty!
 ) Paste code into text editor
 ) Provide parameters
     - target_urls
     - start_urls
     - handle_httpstatus_list
+    - report
 ) Save as main.py
 ) Open a terminal
 ) Set up and activate a python virtual environment
 ) Install scrapy with: $ pip install scrapy
 ) Run the crawler with: $ scrapy runspider script.py -o <insert_report_name.csv>
+
 """
 
 from scrapy.spiders import CrawlSpider, Rule
@@ -35,9 +37,9 @@ class MyItems(Item):
 #
 class Spider(CrawlSpider):
     name = "Broken Link Crawler"
-    target_domains = ["corescientific.com"]  # restrict crawling to this domain
-    start_urls = ["https://corescientific.com"]  # start crawling here
-    handle_httpstatus_list = [400, 403, 404, 408, 410, 500, 502, 504, 511]  # specify error codes reported
+    target_domains = ["<insert domain here>"]  # restrict crawling to this domain
+    start_urls = ["<insert starting URL here"]  # start crawling here
+    handle_httpstatus_list = [<insert list of error codes as ints separated by commas>]  # specify error codes reported
     # Throttle http requests to prevent response 429
     custom_settings = {
         'CONCURRENT_REQUESTS': 25,
@@ -59,7 +61,7 @@ class Spider(CrawlSpider):
 
     # method that yields a tuple containing the referer,
     def parse_my_url(self, response):
-        report = [400, 403, 404, 408, 410, 500, 502, 504, 511]  # should match handle_httpstatus_list
+        report = [insert error/response codes separated by a comma]  # should match handle_httpstatus_list
         if response.status in report:  # if the response matches then creates a MyItems
             item = MyItems()
             item['Referer'] = response.request.headers.get('Referer')
